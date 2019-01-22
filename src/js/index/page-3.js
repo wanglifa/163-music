bc07eb60337d46ed49027ca7b8ff96a6612a0330
wanglifa <wanglifa1995@gmail.com>
2019-01-22 23:18:17 +0800

    点击热搜历史记录显示和删除完成

--- src/js/index/page-3.js
+++ src/js/index/page-3.js
@@ -90,12 +90,36 @@
                 `) 
                 this.$el.find('.singer-cover').append($li)
             }
+        },
+        renderHistory(data){
+            this.$el.find('.history-list').html('')
+            data.map(songOrSinger=>{
+                let $li = $(`
+                    <li class="item">
+                        <i>
+                            <svg class="icon" aria-hidden="true">
+                                <use xlink:href="#icon-history"></use>
+                            </svg>
+                        </i>
+                        <div class="item-right">
+                            <a href="${songOrSinger.href}">${songOrSinger.name}</a>
+                            <i class="delete">
+                                <svg class="icon" aria-hidden="true">
+                                    <use xlink:href="#icon-close1"></use>
+                                </svg>
+                            </i>
+                        </div>
+                    </li>
+                `)
+                this.$el.find('.history-list').append($li)
+            })
         }
     }
     let model = {
         data: {
             songs: [],
-            songs2: []
+            songs2: [],
+            historyList: []
         },
         find(str){
             var name = new AV.Query('Song')
@@ -109,7 +133,11 @@
             var query = new AV.Query('Song');
             query.startsWith('singer', singer)
             return query.find()
-        }
+        },
+        updataHistoryList(){
+            console.log(this.data.historyList)
+            localStorage.setItem('search-history',JSON.stringify(this.data.historyList))
+        },
     }
     let controller = {
         view: null,
@@ -125,6 +153,7 @@
             this.$el = this.view.$el
             this.remove()
             this.bindEvent()
+            this.historyListInit()
         },
         find(current){
             return this.model.find(current.val()).then((songs)=>{
@@ -181,6 +210,8 @@
             this.getFocus()
             this.hotSearch()
             this.inputSubmit()
+            this.deleteHistory()
+            this.historyJump()
         },
         singerClick(){
             this.$el.on('click','#searchResult > li',(e)=>{
@@ -192,13 +223,16 @@
         },
         singerDisplayNorepeat(){
             let {songs} = this.repeatData
-            let newStringSongs = songs.map(song=>{
-                return JSON.stringify(song)
+            this.noRepeat(songs,'songs')
+        },
+        noRepeat(lists,key){
+            let newStringLists = lists.map(list=>{
+                return JSON.stringify(list)
             })
-            let newSongs = [...new Set(newStringSongs)].map(song=>{
-                return JSON.parse(song)
+            let newLists = [...new Set(newStringLists)].map(list=>{
+                return JSON.parse(list)
             })
-            this.model.data.songs = newSongs
+            this.model.data[key] = newLists
         },
         singerModelCssInitJudge(){
             this.$h4 = this.$el.find('.singer-cover > h4')
@@ -215,7 +249,11 @@
             })
         },
         clickSingerAfterInputVal(current,tabName){
-            this.val = current.find(tabName).text().trim()
+            if(tabName){
+                this.val = current.find(tabName).text().trim()
+            }else{
+                this.val = current.trim()
+            }
             this.$el.find('input').val(this.val)
             this.removeLabel()
         },
@@ -226,16 +264,17 @@
         },
         hotSearch(){
             this.$el.find('.hot').on('click','li',(e)=>{
-                this.$el.find('.search-start').addClass('hidden')
                 let a = $(e.currentTarget).find('a')
                 let text = a.text()
                 let href = a.attr('href')
                 this.clickSingerAfterInputVal($(e.currentTarget),'a')
                 this.singerDisplay(href,text)
+                this.updataHistoryList({name:text,href})
+                this.model.updataHistoryList()
             })
         },
         singerDisplay(href,val){
-            this.$el.find('input').val(this.val)
+            this.$el.find('.search-start').addClass('hidden')
             if(href === 'javascript:;'){
                 this.$el.find('#searchResult').empty()
                 this.singerFindSong(val)
@@ -263,7 +302,12 @@
         },
         remove(){
             this.renderInput()
-            this.model.data = {songs:[],songs2:[]}
+            this.$data = this.model.data
+            for(let key in this.$data){
+                if(key !== 'historyList'){
+                    this.$data[key] = []
+                }
+            }
             this.view.render(this.model.data)
             this.view.renderSinger(this.model.data)
             this.$el.find('#searchResult').html('')
@@ -293,6 +337,40 @@
                             this.singerFindSong(this.inputVal)
                         }
                     })
+                }
+            })
+        },
+        historyListInit(){
+            this.historyListData = this.model.data.historyList
+            if(!localStorage.getItem('search-history')){
+                this.model.updataHistoryList()
+            }
+            this.$data.historyList = JSON.parse(localStorage.getItem('search-history'))
+            this.view.renderHistory(this.$data.historyList)
+        },
+        updataHistoryList(data){
+            this.$data.historyList.push(data)
+            this.noRepeat(this.$data.historyList,'historyList')
+            this.view.renderHistory(this.$data.historyList)
+        },
+        historyJump(){
+            this.$el.find('.history-list').on('click','a',(e)=>{
+                let val = $(e.currentTarget).text()
+                let href = $(e.currentTarget).attr('href')
+                this.singerDisplay(href,val)
+                this.clickSingerAfterInputVal(val)
+            })
+        },
+        deleteHistory(){
+            this.$el.find('.history-list').on('click','.delete',(e)=>{
+                let current = $(e.currentTarget).parent().find('a').text()
+                for(let i =0;i<this.$data.historyList.length;i++){
+                    if(this.$data.historyList[i].name === current){
+                        this.$data.historyList.splice(i,1)
+                        this.view.renderHistory(this.$data.historyList)
+                        this.model.updataHistoryList()
+                        break
+                    }
                 }
             })
         }
